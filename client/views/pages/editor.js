17e9408404928243afe49bf8a2682828460ad9e9
Clarence Ngoh <clarencenpy@gmail.com>
2015-06-04 02:12:12 +0800

    added editor ui and submitCode workflow

---
+++ client/views/pages/editor.js
@@ -0,0 +1,51 @@
+Template.editor.onRendered (function () {
+    var questionId = '001';
+    var attempt = Attempts.findOne({userId: Meteor.userId(), questionId: questionId});
+
+    if (!attempt) { //no such record yet
+        var attemptId = Attempts.insert({
+            userId: Meteor.userId(),
+            questionId: questionId
+        });
+        Session.set('curAttempt', attemptId);
+    } else {
+        Session.set('curAttempt', attempt._id);
+    }
+
+});
+
+Template.editor.events({
+    'click #compile-btn': function () {
+        //retrieve editor contents
+        var editor = ace.edit('editor');
+        var code = editor.getSession().getValue();
+
+        Meteor.call('compile', {
+            attemptId: Session.get('curAttempt'),
+            classname: 'Adder',
+            code: code
+        }, function (err, result) {
+            if (err) {
+                console.log(err.error);
+                return;
+            }
+            console.log(result);
+        });
+    }
+});
+
+Template.editor.helpers({
+    config: function () {
+        return function(editor) {
+            editor.setTheme('ace/theme/crimson_editor');
+            editor.getSession().setMode('ace/mode/java');
+            editor.setHighlightActiveLine(false);
+            editor.setShowPrintMargin(false);
+            editor.getSession().setUseWrapMode(true);
+        }
+    },
+
+    docid: function () {
+        return '0001';
+    }
+});
---
+++ server/publications/attempts.js
@@ -0,0 +1,5 @@
+Attempts.allow({
+    insert: function (userId, doc) {
+        return true;
+    }
+})
