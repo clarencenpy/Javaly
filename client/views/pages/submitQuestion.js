44011812f09f06a4c808ba0c899ef9a3bc0d921f
Clarence Ngoh <clarencenpy@gmail.com>
2015-06-06 14:00:55 +0800

    corrected incorrect syntax for Router.go(), and added modal for instructions

--- client/views/pages/submitQuestion.js
+++ client/views/pages/submitQuestion.js
@@ -1,8 +1,20 @@
 AutoForm.hooks({
     insertQuestionForm: {
         onSuccess: function (formType, result) {
-            Router.go('uploadCode/' + result); //result is the questionId
-            //TODO: route needs time to be detected
+            // result is the questionId
+
+            //Add listeners with jquery instead of template.events() so we can have access to the result
+            var $proceedModal =  $('#proceedModal');
+            $proceedModal.modal();
+            $proceedModal.find('#proceed-btn').on('click', function () {
+                $proceedModal.modal('hide');    //the backdrop will only be hidden after the hide animation
+                Router.go('uploadCode', {id: result});
+            });
+            $proceedModal.find('#nowNow-btn').on('click', function () {
+                $proceedModal.modal('hide');
+                Router.go('/');
+            })
+
         }
     }
 });
