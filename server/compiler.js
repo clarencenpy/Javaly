4b03e52e1e2a48eb502848e33545745d19fd78bb
Clarence Ngoh <clarencenpy@gmail.com>
2015-06-13 15:35:48 +0800

    Added time logs to compiler -first run takes a long time as the server restarts on changes to filesystem

--- server/compiler.js
+++ server/compiler.js
@@ -1,47 +1,56 @@
+/*
+ == Fibers and Async ==
+ We are unable to use async functions from npm straight, as the results are delivered
+ through a callback. But this means that the meteor method is unable to return
+ the results from the callback.
+ Meteor provides an abstraction over some boilerplate we have to write involving
+ the use of fibers. Find out more here: https://www.eventedmind.com/feed/nodejs-introducing-fibers
+
+ Limitation: the callback should only take (err, result) as arguments.
+ But thankfully we don't need the 3rd argument, stderr here.
+
+ If we really need to access the other arguments, we can create another wrapper.
+
+ execWrapper = function (cmd, callback) {
+    this.exec(cmd, function(err, stdout, stderr) {
+        var data = {stderr: stderr, stdout: stdout};
+        callback(err, data);
+    })
+ };
+
+*/
+
 exec = Npm.require('child_process').exec;
 fs = Npm.require('fs');
-//mkdirp = Npm.require('mkdirp');
-
-/*
-    == Fibers and Async ==
-    We are unable to use async functions from npm straight, as the results are delivered
-    through a callback. But this means that the meteor method is unable to return
-    the results from the callback.
-    Meteor provides an abstraction over some boilerplate we have to write involving
-    the use of fibers. Find out more here: https://www.eventedmind.com/feed/nodejs-introducing-fibers
-
-    Limitation: the callback should only take (err, result) as arguments.
-    But thankfully we don't need the 3rd argument, stderr here.
-
-    If we really need to access the other arguments, we can create another wrapper.
-
-    execWrapper = function (cmd, callback) {
-        this.exec(cmd, function(err, stdout, stderr) {
-            var data = {stderr: stderr, stdout: stdout};
-            callback(err, data);
-        })
-    };
-
- */
 execSync = Meteor.wrapAsync(exec);
 writeFileSync = Meteor.wrapAsync(fs.writeFile);
+//mkdirp = Npm.require('mkdirp');
 //mkdirpSync = Meteor.wrapAsync(mkdirp);
 
 Meteor.methods({
 
     compileAndRun: function (options) {
 
+        var start = new Date();
+        console.log("Start: " + (new Date() - start));
+
+
+        //  -------- update db ----------- //
         var attempt = Attempts.findOne(options.attemptId);
         var currentCode = attempt.code;
         if (currentCode !== options.code) {    //changes to the code
             Attempts.update(options.attemptId, {$set: {code: options.code}});
         }
-
         var question = Questions.findOne(attempt.questionId);
 
-        //TODO: undo hardcoding
-        //write the code into file
         var attemptDir = process.env.PWD + '/uploads/attempts/' + options.attemptId;
+        var userFilePath = attemptDir + '/' + question.classname + '.java';
+        var engineCP = process.env.PWD + '/java';
+        var questionCP = process.env.PWD + '/uploads/questions/' + attempt.questionId;
+
+
+        //  -------- mkdir ----------- //
+        console.log("start mkdir: " + (new Date() - start));
 
         try {
             execSync('mkdir -p ' + attemptDir); //TODO: there should be a better way instead of exec
@@ -49,21 +58,25 @@ Meteor.methods({
             throw new Meteor.Error(err.message);
         }
 
-        var userFilePath = attemptDir + '/' + question.classname + '.java';
+        console.log("end mkdir, start writing file: " + (new Date() - start));
 
+
+
+        //  -------- write to java file ----------- //
         try {
             writeFileSync(userFilePath, options.code);
         } catch (err) {
             throw new Meteor.Error(err.message);
         }
 
-        var engineCP = process.env.PWD + '/java';
-        var questionCP = process.env.PWD + '/uploads/questions/' + attempt.questionId;
+        console.log("finish writing file, start compile: " + (new Date() - start));
 
-        //compile question dir against attempt dir
+
+
+        //  -------- compile ----------- //
         var args = ['javac', '-cp'];
         args.push(attemptDir + ':' + engineCP + ':' + questionCP);
-        args.push(userFilePath);
+        args.push(questionCP + '/Test.java');
         var cmd = args.join(' ');
 
         try {
@@ -76,19 +89,25 @@ Meteor.methods({
             throw new Meteor.Error(message);
         }
 
-        //run test
+        console.log("finish compile, start run java: " + (new Date() - start));
+
+
+        //  -------- run test ----------- //
         args = ['java', '-cp'];
-        args.push(attemptDir + ':' + process.env.PWD + '/uploads/questions/' + attempt.questionId  + ':' + engineCP);
+        args.push(attemptDir + ':' + questionCP + ':' + engineCP);
         args.push('TestEngine');
         cmd = args.join(' ');
 
         try {
-            return execSync(cmd);
+            var toReturn = execSync(cmd);
+            console.log("done: " + (new Date() - start));
+            return toReturn;
         } catch (err) {
+            console.log("done: " + (new Date() - start));
             throw new Meteor.Error(err.message);
         }
-    }
 
+    }
 
 });
 
