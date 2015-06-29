2a08aebc28668c95b997297f14e1bea9262d0621
Clarence Ngoh <clarencenpy@gmail.com>
2015-06-30 02:34:51 +0800

    compileAndRun method now sends a new job to kue.js

--- server/compiler.js
+++ server/compiler.js
@@ -19,13 +19,17 @@
  };
 
 */
-
+var Future = Npm.require("fibers/future");
 exec = Npm.require('child_process').exec;
 fs = Npm.require('fs');
 execSync = Meteor.wrapAsync(exec);
 writeFileSync = Meteor.wrapAsync(fs.writeFile);
+readFileSync = Meteor.wrapAsync(fs.readFile);
 //mkdirp = Npm.require('mkdirp');
 //mkdirpSync = Meteor.wrapAsync(mkdirp);
+
+kue = Meteor.npmRequire('kue');
+jobs = kue.createQueue();
 
 Meteor.methods({
 
@@ -43,69 +47,95 @@ Meteor.methods({
         }
         var question = Questions.findOne(attempt.questionId);
 
-        var attemptDir = process.env.PWD + '/uploads/attempts/' + options.attemptId;
-        var userFilePath = attemptDir + '/' + question.classname + '.java';
-        var engineCP = process.env.PWD + '/java';
-        var questionCP = process.env.PWD + '/uploads/questions/' + attempt.questionId;
-
-
-        //  -------- mkdir ----------- //
-        console.log("start mkdir: " + (new Date() - start));
-
+        //  -------- add job ----------- //
+        //TODO: should not even write test to file
         try {
-            execSync('mkdir -p ' + attemptDir); //TODO: there should be a better way instead of exec
+            var test = readFileSync(process.env.PWD + '/uploads/questions/' + attempt.questionId + '/Test.java', {encoding: 'utf8'});
         } catch (err) {
             throw new Meteor.Error(err.message);
         }
 
-        console.log("end mkdir, start writing file: " + (new Date() - start));
+        var future = new Future();
+
+        jobs.create('compileRun', {
+            attemptId: options.attemptId,
+            classname: question.classname,
+            code: options.code,
+            test: test
+        })
+        .on('complete', function (result) {
+            console.log("Completed: " + (new Date() - start));
+            future.return(result);
+        })
+        .on('failed', function (err) {
+            console.log("Failed: " + (new Date() - start));
+            future.return(err);
+        })
+        .save();
+
+        return future.wait();   //this is necessary for executing async code in a meteor method
 
 
 
-        //  -------- write to java file ----------- //
-        try {
-            writeFileSync(userFilePath, options.code);
-        } catch (err) {
-            throw new Meteor.Error(err.message);
-        }
+        // the code below has been replaced using kue
 
-        console.log("finish writing file, start compile: " + (new Date() - start));
-
-
-
-        //  -------- compile ----------- //
-        var args = ['javac', '-cp'];
-        args.push(attemptDir + ':' + engineCP + ':' + questionCP);
-        args.push(questionCP + '/Test.java');
-        var cmd = args.join(' ');
-
-        try {
-            execSync(cmd);
-        } catch (err) {
-            //strip the filepath in front
-            var message = err.message;
-            var index = message.indexOf(question.classname + '.java');
-            message = message.substring(index);
-            throw new Meteor.Error(message);
-        }
-
-        console.log("finish compile, start run java: " + (new Date() - start));
-
-
-        //  -------- run test ----------- //
-        args = ['java', '-cp'];
-        args.push(attemptDir + ':' + questionCP + ':' + engineCP);
-        args.push('TestEngine');
-        cmd = args.join(' ');
-
-        try {
-            var toReturn = execSync(cmd);
-            console.log("done: " + (new Date() - start));
-            return toReturn;
-        } catch (err) {
-            console.log("done: " + (new Date() - start));
-            throw new Meteor.Error(err.message);
-        }
+        ////  -------- mkdir ----------- //
+        //console.log("start mkdir: " + (new Date() - start));
+        //
+        //try {
+        //    execSync('mkdir -p ' + attemptDir); //TODO: there should be a better way instead of exec
+        //} catch (err) {
+        //    throw new Meteor.Error(err.message);
+        //}
+        //
+        //console.log("end mkdir, start writing file: " + (new Date() - start));
+        //
+        //
+        //
+        ////  -------- write to java file ----------- //
+        //try {
+        //    writeFileSync(userFilePath, options.code);
+        //} catch (err) {
+        //    throw new Meteor.Error(err.message);
+        //}
+        //
+        //console.log("finish writing file, start compile: " + (new Date() - start));
+        //
+        //
+        //
+        ////  -------- compile ----------- //
+        //var args = ['javac', '-cp'];
+        //args.push(attemptDir + ':' + engineCP + ':' + questionCP);
+        //args.push(questionCP + '/Test.java');
+        //var cmd = args.join(' ');
+        //
+        //try {
+        //    execSync(cmd);
+        //} catch (err) {
+        //    //strip the filepath in front
+        //    var message = err.message;
+        //    var index = message.indexOf(question.classname + '.java');
+        //    message = message.substring(index);
+        //    throw new Meteor.Error(message);
+        //}
+        //
+        //console.log("finish compile, start run java: " + (new Date() - start));
+        //
+        //
+        ////  -------- run test ----------- //
+        //args = ['java', '-cp'];
+        //args.push(attemptDir + ':' + questionCP + ':' + engineCP);
+        //args.push('TestEngine');
+        //cmd = args.join(' ');
+        //
+        //try {
+        //    var toReturn = execSync(cmd);
+        //    console.log("done: " + (new Date() - start));
+        //    return toReturn;
+        //} catch (err) {
+        //    console.log("done: " + (new Date() - start));
+        //    throw new Meteor.Error(err.message);
+        //}
 
     }
 
