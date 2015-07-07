967f8f3d4c1b19b13251e9beb1ebd435206a1331
Clarence Ngoh <clarencenpy@gmail.com>
2015-07-08 03:54:12 +0800

    now instructors can also see attempted questions in the progress bar

--- client/views/pages/groupDashboard/exerciseListing.js
+++ client/views/pages/groupDashboard/exerciseListing.js
@@ -1,19 +1,40 @@
-Template.exerciseListing.helpers({
-    completedPercentage: function () {
-        var exercise = this;
-        var count = 0;
+Template.exerciseListing.onCreated(function () {
+
+    var instance = this;
+    instance.completedPercentage = new ReactiveVar();
+    instance.attemptedButNotCompletedPercentage = new ReactiveVar();
+
+    Tracker.autorun(function () {
+        var exercise = Template.currentData();
         var group = Template.parentData(1);
+        var completed = 0;
+        var attempted = 0;
         _.each(group.participants, function (userId) {
             _.each(exercise.questions, function (questionId) {
                 var attempt = Attempts.findOne({userId: userId, questionId: questionId});
-                if (attempt && attempt.result && attempt.result.success) {
-                    count++;
+                if (attempt && attempt.history) {  //have attempted before
+                    attempted++;
+                    if (attempt.result.success){
+                        completed++;
+                    }
                 }
             });
         });
 
         var total = group.participants.length * exercise.questions.length;
+        instance.completedPercentage.set(Math.round(completed/total*100));
+        instance.attemptedButNotCompletedPercentage.set(Math.round((attempted-completed)/total*100));
 
-        return Math.round(count/total*100);
+    });
+});
+
+
+
+Template.exerciseListing.helpers({
+    completedPercentage: function () {
+        return Template.instance().completedPercentage.get();
+    },
+    attemptedButNotCompletedPercentage: function () {
+        return Template.instance().attemptedButNotCompletedPercentage.get();
     }
 });
