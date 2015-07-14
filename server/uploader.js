8b0e1fb4dff8dd78a9934aa88918e01b3b43618c
Clarence Ngoh <clarencenpy@gmail.com>
2015-07-14 23:07:00 +0800

    disable uploading functionality

--- server/uploader.js
+++ server/uploader.js
@@ -1,15 +1,15 @@
-Meteor.startup(function () {
-
-    //init code for upload-server package
-    UploadServer.init({
-        tmpDir: process.env.PWD + '/uploads/tmp',
-        uploadDir: process.env.PWD + '/uploads/',
-        checkCreateDirectories: true, //create the directories for you
-        getDirectory: function (fileInfo, formData) {
-            return 'questions/' + formData.questionId;
-        },
-        finished: function (fileInfo, formData) {
-            Questions.update(formData.questionId, {$set: {uploaded: true}});
-        }
-    })
-});
+//Meteor.startup(function () {
+//
+//    //init code for upload-server package
+//    UploadServer.init({
+//        tmpDir: process.env.PWD + '/uploads/tmp',
+//        uploadDir: process.env.PWD + '/uploads/',
+//        checkCreateDirectories: true, //create the directories for you
+//        getDirectory: function (fileInfo, formData) {
+//            return 'questions/' + formData.questionId;
+//        },
+//        finished: function (fileInfo, formData) {
+//            Questions.update(formData.questionId, {$set: {uploaded: true}});
+//        }
+//    })
+//});
