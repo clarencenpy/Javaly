2087df7d63fd588dd8fe58ead3f0c0a574c0bc66
Clarence Ngoh <clarencenpy@gmail.com>
2015-08-16 08:43:07 +0800

    ui tweaks for manageGroups, added undeleteable warning

--- client/views/pages/manageGroups/manageGroups.js
+++ client/views/pages/manageGroups/manageGroups.js
@@ -12,18 +12,32 @@ Template.manageGroups.events({
     'click .add-exercise-btn': function () {
         var groupId = this._id;
         var exerciseId = Random.id();
-        Groups.update({_id: groupId}, {$push: {
-            exercises: {
-                _id: exerciseId,
-                description: "",
-                questions: [],
-                createdAt: new Date()
-            }
-        }});
 
-        Router.go('editExercise', {
-            groupId: groupId,
-            exerciseId: exerciseId
-        })
+        swal({
+            title: "Are you sure?",
+            text: "You cannot delete an exercise after it is created (for now), as it might interrupt students already working on it.",
+            type: "info",
+            showCancelButton: true,
+            confirmButtonText: "Proceed",
+            cancelButtonText: "Cancel"
+        }, function () {
+            //create a new exercise before redirecting to exercise builder
+
+            Groups.update({_id: groupId}, {
+                $push: {
+                    exercises: {
+                        _id: exerciseId,
+                        description: "",
+                        questions: [],
+                        createdAt: new Date()
+                    }
+                }
+            });
+
+            Router.go('editExercise', {
+                groupId: groupId,
+                exerciseId: exerciseId
+            })
+        });
     }
 });
