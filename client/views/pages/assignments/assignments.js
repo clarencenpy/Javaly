2aaa0ab09e91252f437c95d32d1b4f0521708862
Clarence Ngoh <py.ngoh.2014@smu.edu.sg>
2015-09-05 20:24:39 +0800

    resolve bug: now adds current exercise to session when clicking on question that has been attempted before

--- client/views/pages/assignments/assignments.js
+++ client/views/pages/assignments/assignments.js
@@ -115,5 +115,20 @@ Template.assignments.events({
         } else {
             Router.go('codepad', {id: attempt._id});
         }
+    },
+    'click .view-btn': function () {
+        Session.set('currentExercise', Template.instance().questionsFromSelectedExercise.get());
+        var questionId = this.questionId;
+        //check if previous attempt exists
+        var attempt =  Attempts.findOne({questionId: questionId, userId: Meteor.userId()});
+        if (attempt === undefined) {
+            var attemptId = Attempts.insert({
+                userId: Meteor.userId(),
+                questionId: questionId
+            });
+            Router.go('codepad', {id: attemptId});
+        } else {
+            Router.go('codepad', {id: attempt._id});
+        }
     }
 });
