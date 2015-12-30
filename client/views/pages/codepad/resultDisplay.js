b2eefca1026b3559c9917903fbd888c448c997a6
Clarence Ngoh <py.ngoh.2014@smu.edu.sg>
2015-12-30 18:56:30 +0800

    ui for result display now shows overall pass score

--- client/views/pages/codepad/resultDisplay.js
+++ client/views/pages/codepad/resultDisplay.js
@@ -1,9 +1,20 @@
 Template.resultDisplay.helpers({
-   quoteIfString: function (s) {
-       if (typeof s === 'string') {
-           return '"' + s + '"';
-       } else {
-           return s;
-       }
-   }
+    quoteIfString: function (s) {
+        if (typeof s === 'string') {
+            return '"' + s + '"';
+        } else {
+            return s;
+        }
+    },
+    pass: function () {
+        var results = Template.instance().data;
+        var output = '';
+        output += _.filter(results.runs, function (run) {
+            return run.result.success;
+        }).length;
+        output += '/';
+        output += results.runs.length;
+        return output;
+    }
 });
+
