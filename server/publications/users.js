b7bf86c49d6e2ca835aad4316c147eed951406b6
Clarence Ngoh <py.ngoh.2014@smu.edu.sg>
2016-01-07 15:12:20 +0800

    minor fixes

--- server/publications/users.js
+++ server/publications/users.js
@@ -1,13 +1,9 @@
 Meteor.publish('allUsers', function () {
-    if (Roles.userIsInRole(this.userId, ['admin', 'instructor', 'ta'])) {
-        // publishing all users instead of by role, as performing a full search for users in role is expensive
-        return Meteor.users.find({}, {fields: {
-            profile: 1,
-            emails: 1,
-            roles: 1,
-            'services.google.email': 1
-        }});
-    } else {
-        this.stop();
-    }
+    // publishing all users instead of by role, as performing a full search for users in role is expensive
+    return Meteor.users.find({}, {fields: {
+        profile: 1,
+        emails: 1,
+        roles: 1,
+        'services.google.email': 1
+    }});
 });
