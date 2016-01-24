227a4c208b9584c0001d746bfd171b6f96edb97b
clarencenpy <clarencenpy@gmail.com>
2016-01-24 11:30:35 +0800

    deprecated old group dashboard

--- client/views/pages/groupDashboard/groupView.js
+++
@@ -1,9 +0,0 @@
-Template.groupView.helpers({
-    selectedAttempt: function () {
-        return Session.get('selectedAttempt');
-    }
-});
-
-Template.groupView.onDestroyed(function () {
-    Session.set('selectedAttempt', null);
-});
